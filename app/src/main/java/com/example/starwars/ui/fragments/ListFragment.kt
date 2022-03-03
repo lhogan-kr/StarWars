5adb56c41ab11323dd9bff134de699868d67a4ac
lhogan-kr <lane.hogan@kroger.com>
2022-03-03 15:53:14 -0500

    Add transition between list screen and detail screen

--- app/src/main/java/com/example/starwars/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/example/starwars/ui/fragments/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -24,6 +25,7 @@ class ListFragment : Fragment() {
 
         activity?.supportFragmentManager?.commit {
             setReorderingAllowed(true)
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             replace(R.id.fragment_container, detailFragment)
             addToBackStack(null)
         }
